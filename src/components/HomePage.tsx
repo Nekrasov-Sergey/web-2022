51e4b4592236b3002bd8d2ab7d7b87472c827992
Sergey <79508031750@yandex.ru>
2022-10-31 23:47:08 +0300

    [lab4] added path

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -4,8 +4,11 @@ import {Promo} from "./Promo";
 export function HomePage() {
     return (
         <div className="bg-yellow-50">
-            <p className="text-center text-6xl font-bold mb-6 text-pink-500	whitespace-pre-wrap">
-                Доступные промокоды на Октябрь 2022
+            <p className="ml-4 text-2xl font-normal text-black">
+                Freebie shop
+            </p>
+            <p className="text-center text-6xl font-bold text-pink-500">
+                Доступные промокоды на Ноябрь 2022
             </p>
             {Promos.map((promo) => {
                 return <Promo promo={promo}/>
--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -3,19 +3,23 @@ import {useLocation} from "react-router-dom";
 
 export function Payment() {
     return (
-        <div className="text-center">
-            <p className=" font-bold text-6xl text-pink-500">
+        <div className="bg-yellow-50">
+            <p className="ml-4 text-2xl font-normal text-black">
+                Freebie shop / payment
+            </p>
+
+            <p className=" font-bold text-6xl text-pink-500 text-center">
                 Страница оплаты
             </p>
 
-            <p className="mt-8 font-medium text-4xl text-green-500">
+            <p className="mt-8 font-medium text-4xl text-green-500 text-center">
                 Ваш промокод:
                 <p className="font-bold italic text-4xl text-red-700">
                     {useLocation().state.promo}
                 </p>
             </p>
 
-            <p className="mt-8">
+            <p className="my-8 text-center">
                 <Link to="/"
                       className="border-4 border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white py-1 px-3 rounded-full text-2xl font-bold"
                 >
@@ -23,6 +27,7 @@ export function Payment() {
                 </Link>
             </p>
 
+            <img src="/resourses/Payment.jpg" width="23%" className="mx-auto" alt="Payment"/>
         </div>
     )
 }
